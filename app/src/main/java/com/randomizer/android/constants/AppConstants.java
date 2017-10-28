c062b20e1bcc561bc7f805622546523ea36bef8c
Tahir <tahir90.webdeveloper@gmail.com>
2017-10-28 14:41:13 +0400

    Changes page_count limit

--- app/src/main/java/com/randomizer/android/constants/AppConstants.java
+++ app/src/main/java/com/randomizer/android/constants/AppConstants.java
@@ -9,6 +9,6 @@ public class AppConstants {
     public static final String RANDOM_USER_API_URL = "https://randomuser.me/api/";
     public static final String SHARED_PREFS_NAME = "SharedPref";
     public static final int DEFAULT_PAGE_SIZE = 50;
-    public static final int LAST_PAGE_NUMBER = 250; // Setting for first 5000 users
+    public static final int LAST_PAGE_NUMBER = 100; // Setting for first 5000 users
     public static final int FIRST_PAGE_NUMBER = 1;
 }
